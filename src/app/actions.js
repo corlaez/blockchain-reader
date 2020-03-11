891c4986e29098cca9a995215cc23105baf5fe25
Armando <armanpela@gmail.com>
2020-03-11 10:03:05 -0400

    Enhancements in actions

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,7 +1,3 @@
-export const setLoading = ({ state }, payload) => {
-  state.loading = payload;
-};
-
 export const updateLatest = async ({ state, effects }) => {
   if (state.loading) return;
   state.loading = true;
@@ -24,22 +20,18 @@ export const setTxHash = ({ state }, value) => {
   state.txHash = value;
 };
 
-export const setBlocks = ({ state }, value) => {
-  state.blocks = value;
-};
-
-export const appendBlock = ({ state }, value) => {
-  state.blocks.push(value);
-};
-
 export const loadPrevious = async ({ state, effects }) => {
   if (state.loading) return;
   state.loading = true;
   const blocks = state.blocks;
   const prevBlock = blocks[blocks.length - 1].prev_block;
-  const block = await effects.getBlock(prevBlock);
-  state.loading = false;
-  state.blocks.push(block);
+  try {
+    const block = await effects.getBlock(prevBlock);
+    state.blocks.push(block);
+  } catch (e) {
+  } finally {
+    state.loading = false;
+  }
 };
 
 export const loadTransaction = async ({ state, effects }) => {
@@ -49,7 +41,10 @@ export const loadTransaction = async ({ state, effects }) => {
     const tx = await effects.getTransaction(state.txHash);
     state.transaction = tx;
   } catch (e) {
-    state.txHash = state.transaction.hash;
+    if (state.transaction)
+      state.txHash = state.transaction.hash;
+    else
+      state.txHash = ""
   } finally {
     state.loading = false;
   }
@@ -58,7 +53,6 @@ export const loadTransaction = async ({ state, effects }) => {
 export const changeBlockHash = async ({ state, effects }, e) => {
   if (state.loading) return;
   const blockHash = e.currentTarget.value;
-  const oldBlockHash = state.blockHash;
   state.blockHash = blockHash;
   const firstBlock = state.blocks[0];
   if (blockHash !== firstBlock.hash && blockHash.length >= 64) {
@@ -69,7 +63,7 @@ export const changeBlockHash = async ({ state, effects }, e) => {
       state.loading = false;
     } catch (e) {
     } finally {
-      state.blockHash = oldBlockHash;
+      state.blockHash = firstBlock.hash;
       state.loading = false;
     }
   }
